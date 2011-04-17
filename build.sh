7533cf88d1cf786d96ff0f652856872285bcd330
Unsekure Git <git@unsekure.net>
2011-04-17 14:19:08 -0400

    Refactoring to support NotifyMyAndroid

---
+++ build.sh
@@ -0,0 +1,1 @@
+gcc -o nma -lssl -lcrypto nma.c main.c
--- main.c
+++ main.c
@@ -5,21 +5,22 @@
  * can do whatever you want with this stuff. If we meet some day, and you think
  * this stuff is worth it, you can buy me a beer in return J. Dijkstra (04/29/2010)
  * ----------------------------------------------------------------------------
+ *  Modified by Adriano Maia (adriano@usk.bz)
  */
-#include "prowl.h"
+#include "nma.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-#define APP_NAME "ProwlExample"
+#define APP_NAME "NotifyMyAndroidExample"
 
 int main(int argc, char** argv) {	
 	if (argc < 4)
 	{
-		printf("Usage: prowl <api_key> <priority-number> <event> <description>\n");
+		printf("Usage: nma <api_key> <priority-number> <event> <description>\n");
 		return 1;
 	}
 	
-	printf("Return code: %d\n", prowl_push_msg(argv[1], atoi(argv[2]), APP_NAME, argv[3], argv[4]));
+	printf("Return code: %d\n", nma_push_msg(argv[1], atoi(argv[2]), APP_NAME, argv[3], argv[4]));
 
     return 0;
 }
