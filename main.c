57634d5307e0271dcfb4ee21f90c133875d0c68a
pinguinfuss <christian.heim@barfoo.org>
2013-02-24 11:47:34 +0000

    Rework the main.c to have command line switch parsing and loading the API key from an external file.

--- main.c
+++ main.c
@@ -5,36 +5,126 @@
  * can do whatever you want with this stuff. If we meet some day, and you think
  * this stuff is worth it, you can buy me a beer in return J. Dijkstra (04/29/2010)
  * ----------------------------------------------------------------------------
- *  Modified by Adriano Maia (adriano@usk.bz)
+ *  Modified by Christian Heim (christian.heim@barfoo.org)
  */
 #include "nma.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
 
-#define APP_NAME "NotifyMyAndroidExample"
+void usage() {
+	printf("nma, version: %s\n", NMA_VERSION);
+	printf("\n");
+	printf("Required arguments:\n");
+	printf("--apifile\n");
+	printf("\tPath to a the apikey file (see below)\n");
+	printf("\n--app\n");
+	printf("\tName of the application submitting the notification.\n\tIf no value has been passed, it'll default to NMA-for-C.\n\tMax length: 256\n");
+	printf("\n--priority\n");
+	printf("\tPriority as an integer value\n\tPossible values: -2, ..., 2\n\tSee nma.h for the corresponding levels.\n");
+	printf("\n--event\n");
+	printf("\tSubject of the notification.\n\tMax length: 1000\n");
+	printf("\n--description\n");
+	printf("\tNotification text.\n\tMax length: 10000\n");
+	printf("\nThe API key configration file should look like this:\n");
+	printf("apikey = asehukf348cge1oxhd2ysvn06hlyq6y5kmzes604jr7fjdq7\n");
+}
 
-int main(int argc, char** argv) {
-	FILE * file = fopen( argv[1], "r");
+int main(int argc, char *argv[]) {
+
+	int getopts;
+	char *filename;
+	char *app;
+	unsigned int priority;
+	char *event;
+	char *description;
 	char apikey[48];
+	int verbose = 0;
 
-	if (argc < 4)
+	static struct option long_options[] =
 	{
-		printf("Usage: nma /api/config/file <priority-number> <event> <description>\n");
-		printf("\nThe API key configration file should look like this:\n");
-		printf("apikey = asehukf348cge1oxhd2ysvn06hlyq6y5kmzes604jr7fjdq7\n");
-		return 1;
-	}
+		{"apifile", required_argument, NULL, 'f'},
+		{"app", required_argument, NULL, 'a'},
+		{"priority", required_argument, NULL, 'p'},
+		{"event", required_argument, NULL, 'e'},
+		{"description", required_argument, NULL, 'd'},
+		{"verbose", no_argument, 0, 'v'},
+		{"help", no_argument, NULL, 'h'}
+	};
 
-	if ( fscanf(file, "apikey = %s", &apikey) == 1 )
+	if ( argc == 1 )
 	{
-		printf("Return code: %d\n", nma_push_msg(apikey, atoi(argv[2]), APP_NAME, argv[3], argv[4]));
-		return 0;
+		usage();
+		exit(1);
 	}
 	else
 	{
-		printf("Unable to open/invalid syntax config file: %s", file);
-		return 1;
-	}
-	
+		while (( getopts = getopt_long(argc, argv, "f:a:p:e:d", long_options, NULL)) != -1)
+		{
+			switch (getopts)
+			{
+				case 'h':
+					usage();
+					exit(1);
+				case 'v':
+					verbose = 1;
+					break;
+				case 'f':
+					filename = optarg;
+					break;
+				case 'a':
+					app = optarg;
+					break;
+				case 'p':
+					priority = atoi(optarg);
+					break;
+				case 'e':
+					event = optarg;
+					break;
+				case 'd':
+					description = optarg;
+					break;
+			}
+		}
 
+		if ( verbose > 0 )
+		{
+			printf("filename: %s\n", filename);
+			printf("app: %s\n", app);
+			printf("priority: %u\n", priority);
+			printf("event: %s\n", event);
+			printf("description: %s\n", description);
+		}
+
+		FILE *fp;
+
+		if (( fp=fopen(filename, "r")) == NULL)
+		{
+			printf("Unable to open config file: %s\n", filename);
+			return 1;
+		}
+		else
+		{
+			if ( fscanf(fp, "apikey = %s", &apikey) == 1 )
+			{
+				if ( verbose > 0 )
+				{
+					printf("apikey: %s\n", apikey);
+					printf("Return code: %d\n", nma_push_msg(apikey, priority, app, event, description));
+					return 0;
+				}
+				else
+				{
+					nma_push_msg(apikey, priority, app, event, description);
+					return 0;
+				}
+			}
+			else
+			{
+				printf("Invalid syntax config file: %s\n", filename);
+				return 1;
+			}
+		}
+		fclose(fp);
+	}
 }
